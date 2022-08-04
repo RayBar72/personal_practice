047d51d3dd9ebefdd63de42caa7cc669fbf19bd8
Raymundo Barrera Flores <91104001+RayBar72@users.noreply.github.com>
2022-08-04 17:27:28 -0300

    Delete cgate_lite_cont/backend directory

--- cgate_lite_cont/backend/cmoney/admin.py
+++
@@ -1,8 +0,0 @@
-'''Register classes in admin webpage'''
-from django.contrib import admin
-from .models import GasStations, Pump, Transactions
-
-
-admin.site.register(GasStations)
-admin.site.register(Pump)
-admin.site.register(Transactions)
--- cgate_lite_cont/backend/cmoney/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CmoneyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cmoney'
