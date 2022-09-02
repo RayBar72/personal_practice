c19c275f41c11ade4e83fdff001184c1de892fa5
RayBar72 <3963@holbertonschool.com>
2022-09-02 17:23:01 -0300

    Backup 220902h1722

---
+++ js_repast/0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const sal = "\n"
+const uno = "C is fun"
+const dos = "Python is cool"
+const tres = "JavaScript is amazing"
+const cuatro = uno + sal + dos + sal + tres
+console.log(cuatro)
